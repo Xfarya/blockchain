e9e7c8a3c9b531a3911b231d3d913a711bbae618
Xfarya <farya243@gmail.com>
2022-05-03 15:21:01 +0100

    added function to replace the chain along with tests

--- blockchain.js
+++ blockchain.js
@@ -23,8 +23,22 @@ class Blockchain {
                 block.hash !== Block.blockHash(block)) {
                 return false;
             }
-            return true;
+       
         }
+        return true;
+    }
+
+    replaceChain(newChain) {
+        if (newChain.length <= this.chain.length) {
+            console.log('Received chain is not longer than current chain.');
+            return;
+        } else if (!this.isValidChain(newChain)) {
+            console.log('The received chain is not valid.');
+            return;
+        } 
+        
+        console.log('Replacing blockchain with the new chain')
+        this.chain = newChain;
     }
 }
 
