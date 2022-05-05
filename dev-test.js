aaf85371fcadba50a5415ac63aec418f6b037ed8
Xfarya <farya243@gmail.com>
2022-05-05 11:44:08 +0100

    complete proof of work system with tests

--- dev-test.js
+++ dev-test.js
@@ -1,5 +1,7 @@
-const Block = require('./block');
+const Blockchain = require('./blockchain');
 
-const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
+const bc = new Blockchain();
 
-console.log(fooBlock);
+for (let i = 0; i < 10; i++) {
+    console.log(bc.addBlock(`foo ${i}`).toString());
+}
